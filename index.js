6ac874eb34ca347c5ad3af617cca7a13cb1de998
Simon van Endern <siva@netlight.com>
2020-02-20 11:42:04 +0100

    Setting up basic router for API

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var express = require('express')
 
 var app = express()
 
+APIrouter = express.Router()
+
+APIrouter.get('*', function (req, res, next) {
+	res.status(200).json({})
+})
+
+app.use('/api', APIrouter)
+
 app.get('*', function (req, res, next) {
 	res.status(200).send("Hello World")
 })
